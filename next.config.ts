b7aaf01d682d79d465e70814ff4902c0933e9d3c
gandy818 <gandy818@gmail.com>
2025-01-01 21:19:28 +0900

    feat: config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
