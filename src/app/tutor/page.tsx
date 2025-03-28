8c0f8394c530b6a9fbca96b3b5c3e7f099077f1e
unknown <rhksgnl11@naver.com>
2025-03-28 13:40:13 +0900

    섹션1 중앙 정렬

--- src/app/tutor/page.tsx
+++ src/app/tutor/page.tsx
@@ -35,7 +35,7 @@ export default function TutorPage() {
         {/* 섹션 1 */}
         <>
           <div className="flex flex-wrap justify-center text-5xl font-bold max-md:flex-col max-md:text-[32px]">
-            <p className="self-center pr-4">TESOL/TEFL을 보유한</p>
+            <p className="self-center md:pr-4">TESOL/TEFL을 보유한</p>
             <p className="self-center text-[#7D5FFF] md:pr-4">100% 미국·캐나다</p>
             <p className="self-center">원어민 강사진</p>
           </div>
@@ -122,7 +122,7 @@ export default function TutorPage() {
         {/* 섹션 2 모바일 */}
         <div className="mt-[60px] md:hidden">
           <div
-            className="flex min-h-[1000px] justify-end bg-cover bg-center"
+            className="bg-co flex min-h-[1000px] justify-end bg-center"
             style={{
               backgroundImage: 'url(/images/mobile-bg-tutor.png)', // 배경 이미지 URL
             }}
