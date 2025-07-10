299038f50e14155157d8f7f697fc19b5d6d4f328
edgeenglish <support@edgeenglish.kr>
2025-07-10 18:36:23 +0900

    '할인 수강권 확인하기'로 텍스트 수정함

--- src/components/FloatingMenu.tsx
+++ src/components/FloatingMenu.tsx
@@ -18,11 +18,10 @@ export default function FloatingMenu() {
     <div>
       {pathName === '/' && (
         <Link
-          href="https://forms.gle/rkvoS9ut3a5eSe659"
-          target="_blank"
+          href="/events"
           className="fixed bottom-4 left-1/2 z-[1] -translate-x-1/2 whitespace-nowrap rounded-full bg-edge-yellow px-10 py-4 font-semibold shadow-md"
         >
-          회원가입 없이 간편 수강신청하기
+          할인 수강권 확인하기
         </Link>
       )}
       <div className="fixed bottom-[4.5rem] right-4.5 z-[19] flex flex-col gap-2 xs:bottom-10 xs:right-10">
